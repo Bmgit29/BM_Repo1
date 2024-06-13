048eb7839b0f418adf0eeef30a55a463e8442246
Bibhu Mishra <bibhuqa29@gmail.com>
2024-06-13 22:47:24 +0530

    Version1

---
+++ MJava.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.Scanner;
+
+class Test{
+public static void main(String[] args)
+{
+ System.out.println("Hello");
+ int a ;
+ int b;
+ int sum;
+ Scanner myobj=new Scanner(System.in);
+ System.out.println("enter 1st number");
+ a=myobj.nextInt();
+ System.out.println("enter 2nd nnumber");
+ b=myobj.nextInt();
+ sum= a+b;
+ System.out.println(sum+"Version 1"
+ );
+}}
